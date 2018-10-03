ea1a3ea95faa65829a591bb5e03cdfc27f68014a
Alex Adamov <alex.v.adamov@gmail.com>
2018-10-03 11:52:31 +0300

    in class phone project goest to heroku

--- src/main/java/controllers/repository/connectionManager/ConnectionManagerJdbcImpl.java
+++ src/main/java/controllers/repository/connectionManager/ConnectionManagerJdbcImpl.java
@@ -23,9 +23,9 @@ public class ConnectionManagerJdbcImpl implements ConnectionManager {
         try {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/mobile",
-                    "postgres",
-                    "1111");
+                    "jdbc:postgresql://ec2-54-246-101-215.eu-west-1.compute.amazonaws.com:5432/dcercrpmh297md?autoReconnect=true&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
+                    "fcagdythznpxgf",
+                    "67b51709816a88426ef72815a349956d7013ca21c77609bee6a21c153689b0c4");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
