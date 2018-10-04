26c4364c6e58d9bd0370a29bef18fb1378890ed4
Alex Adamov <alex.v.adamov@gmail.com>
2018-10-04 10:44:39 +0300

    Randomizer for Mikki v.1.1.1 added excluded numbers

--- src/main/java/controllers/controller/RandomizerServlet.java
+++ src/main/java/controllers/controller/RandomizerServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RandomizerServlet extends HttpServlet {
@@ -18,17 +20,43 @@ public class RandomizerServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String rnd;
+        String rndString;
+        Integer rndInt;
+
+        List<Integer> excluded = parseExcludeString(req.getParameter("excluded"));
         try {
-            rnd = String.valueOf(new Random().nextInt(Integer.parseInt(req.getParameter("count"))) + 1);
+            do {
+
+                rndInt = new Random().nextInt(Integer.parseInt(req.getParameter("count"))) + 1;
+                rndString = String.valueOf(rndInt);
+            } while (excluded.stream().anyMatch(rndInt::equals));
         } catch (NumberFormatException e) {
-            rnd = "Not a number was typed";
+            rndString = "Not a number was typed";
         }
 
 
-        req.setAttribute("enemy", rnd);
+        req.setAttribute("enemy", rndString);
         req.getRequestDispatcher("/random.jsp").forward(req, resp);
     }
 
+    private static List<Integer> parseExcludeString(String s) {
 
+        String[] integerStrings = s.split(" ");
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(0);
+        for (String splited: integerStrings){
+            try {
+                integerList.add(Integer.parseInt(splited));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return integerList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(parseExcludeString("1 2 3 4"));
+    }
 }
+
--- src/main/webapp/random.jsp
+++ src/main/webapp/random.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Rikki
-  Date: 01.10.2018
-  Time: 10:12
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -26,6 +19,8 @@
     <div class="form-group mb-2">
         <input  class="form-control-lg" type="text" name="count" placeholder="People count" autocomplete="off"
         style="font-size: 32px;">
+        <input  class="form-control-lg" type="text" name="excluded" placeholder="excluded numbers from rendomize" autocomplete="off"
+                style="font-size: 32px;">
         <input  class=" btn-lg btn-danger"  type="submit" value="Submit">
     </div>
 </form>
